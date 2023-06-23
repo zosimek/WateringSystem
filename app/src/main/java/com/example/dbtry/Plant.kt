f61ec09df760c16782a1a8ddba179f7e2b172e0a
Zofia Dobrowolska <zof.dobrowolska@gmail.com>
2023-06-23 03:36:12 +0200

    Plant RecyclerView Image set

--- app/src/main/java/com/example/dbtry/Plant.kt
+++ app/src/main/java/com/example/dbtry/Plant.kt
@@ -2,5 +2,5 @@ package com.example.dbtry
 
 import android.hardware.Sensor
 
-data class Plant(var name: String ?=null)
+data class Plant(var name: String ?=null, var image: Int ?=null)
 //, var mode: String ?= null, var pump: Pump ?=null, var sensor: Sensor? = null
--- app/src/main/java/com/example/dbtry/PlantAdapter.kt
+++ app/src/main/java/com/example/dbtry/PlantAdapter.kt
@@ -3,6 +3,7 @@ package com.example.dbtry
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
@@ -30,6 +31,32 @@ class PlantAdapter(private val plantList : ArrayList<Plant>) : RecyclerView.Adap
     override fun onBindViewHolder(holder: PlantViewHolder, position: Int) {
         val currentitem = plantList[position]
         holder.name.text = currentitem.name
+        when (currentitem.image){
+            1 -> {
+                holder.imagePlant.setImageResource(R.drawable.plant_benjaminek)
+            }
+            2 -> {
+                holder.imagePlant.setImageResource(R.drawable.plant_bonsai)
+            }
+            3 -> {
+                holder.imagePlant.setImageResource(R.drawable.plant_kaktus)
+            }
+            4 -> {
+                holder.imagePlant.setImageResource(R.drawable.plant_kwiatek)
+            }
+            5 -> {
+                holder.imagePlant.setImageResource(R.drawable.plant_monstera)
+            }
+            6 -> {
+                holder.imagePlant.setImageResource(R.drawable.plant_sokulent)
+            }
+            7 -> {
+                holder.imagePlant.setImageResource(R.drawable.plant_storczyk)
+            }
+            else -> {
+                holder.imagePlant.setImageResource(R.drawable.plant_benjaminek)
+            }
+        }
     }
 
     override fun getItemCount(): Int {
@@ -39,6 +66,7 @@ class PlantAdapter(private val plantList : ArrayList<Plant>) : RecyclerView.Adap
     class PlantViewHolder(itemView: View, listener: onItemClickListener) : RecyclerView.ViewHolder(itemView){
 
         val name : TextView = itemView.findViewById(R.id.txtPlantName)
+        val imagePlant : ImageView = itemView.findViewById(R.id.imagePlant)
 
         init {
 
